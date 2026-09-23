92d046807e9b6744cbec0438909a58fadea291fb
Bhavya Sharma <bhavyasharmaa67@gmail.com>
2026-09-24 00:41:12 +0530

    updated

--- process_videos.py
+++ process_videos.py
@@ -4,7 +4,7 @@ import subprocess
 files = os.listdir('videos')
 for file in files:
     print(file)
-    tutorial_number = file.split(" [")[0].split(" #")[-1]
+    tutorial_number = file.split(" [")[0].split(" #")[1]
     print(tutorial_number)
     file_name = file.split(" | ")[0]
     print(tutorial_number, file_name)
